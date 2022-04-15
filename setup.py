bdb46f43577911a4ce5986f116f41150f63d63dc
Ther <1329438302@qq.com>
2022-04-15 19:50:30 +0800

    chore: modify setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from importlib_metadata import entry_points
 from setuptools import setup, find_packages
 import os
+import platform
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname),encoding='utf-8').read()
@@ -14,16 +15,7 @@ setup(
     packages=find_packages(),
     license='MIT',
     long_description=read('README.md'),
-
-    entry_points={
-        'console_scripts': [
-            'latexhelper = bin.latex_helper:main'
-        ]
-    },
-
-    scripts=['generate_pdf.sh','generate_ppt.sh'],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console"
-    ],
+    entry_points={'console_scripts': ['latexhelper = bin.latex_helper:main']},
+    scripts=(['generate_pdf.ps1','generate_ppt.ps1'] if (platform.system()== 'Windows') else ['generate_pdf.sh','generate_ppt.sh']),
+    classifiers=["Development Status :: 3 - Alpha", "Environment :: Console"],
 )
